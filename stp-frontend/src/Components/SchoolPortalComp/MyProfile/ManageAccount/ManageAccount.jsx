71b5e398b196158154ee97e1dd42d97d9fcd7d30
poonghy <peterpoong2000@gmail.com>
2024-09-25 10:02:55 +0800

    manageAccount and courseDetail

--- stp-frontend/src/Components/SchoolPortalComp/MyProfile/ManageAccount/ManageAccount.jsx
+++ stp-frontend/src/Components/SchoolPortalComp/MyProfile/ManageAccount/ManageAccount.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Row, Col, Container, Button } from "react-bootstrap";
-import { CheckCircleFill } from "react-bootstrap-icons";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import stickman from "../../../../assets/SchoolPortalAssets/20856932_6381326 1.png";
@@ -12,6 +11,7 @@ const ManageAccount = () => {
   const token = sessionStorage.getItem("token");
   const [basicPackage, setBasicPackage] = useState({});
   const [premiumPackage, setPremiumPackage] = useState({});
+  const [accountType, setAccountType] = useState("");
 
   const getPackage = async (type) => {
     try {
@@ -44,9 +44,36 @@ const ManageAccount = () => {
     }
   };
 
+  const getAccountDetail = async () => {
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_BASE_URL}api/school/schoolDetail`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData["error"] || "Internal Server Error");
+      }
+
+      const fetchedData = await response.json();
+      // setAccountType(fetchedData.data.account_type);
+      setAccountType(65);
+    } catch (error) {
+      console.error("Failed to get package data", error);
+    }
+  };
+
   useEffect(() => {
     getPackage(76); // Basic Package
     getPackage(77); // Premium Package
+    getAccountDetail();
   }, [token]);
 
   const decodeHTML = (html) => {
@@ -78,30 +105,47 @@ const ManageAccount = () => {
           </p>
         </Col>
       </Row>
-      <Row className="d-flex align-items-stretch">
-        <Col md={6}>
-          <img
-            src={stickman}
-            alt="Stickman Illustration"
-            className={styles.leftCornerImage}
-          />
+      <Row className="d-flex align-items-stretch ">
+        <Col md={6} className={styles.alignBottom}>
           <div
-            className={`px-5 py-5  ${styles.packageBasicCard} ${styles.basicPackage}`}
+            className={`px-5 py-5 ${styles.packageBasicCard} ${styles.premiumPackage} `} // Use Premium styles for Basic
+            style={{
+              backgroundColor: accountType === 64 ? "#B71A18" : "white", // Conditional color
+            }}
           >
             <div className="text-start">
-              <img src={basicIcon} alt="basic package icon" />
+              <img
+                src={accountType === 64 ? premiumIcon : basicIcon}
+                alt="basic package icon"
+              />
             </div>
 
-            <h4 className={`mt-3 text-start ${styles.packageName}`}>
+            <h4
+              className={`mt-3 text-start ${styles.packageName}`}
+              style={{
+                color: accountType === 64 ? "white" : "#B71A18", // Conditional color
+              }}
+            >
               {basicPackage.package_name || "Basic Package"}
             </h4>
 
             <div className="d-flex">
-              <button className={`${styles.customBtn}`}>Features</button>
+              <button
+                className={`${styles.customBtn}`}
+                style={{
+                  backgroundColor: accountType === 64 ? "white" : "#B71A18",
+                  color: accountType === 64 ? "#B71A18" : "white",
+                }}
+              >
+                Features
+              </button>
             </div>
 
             <p
-              className="text-start "
+              className="text-start"
+              style={{
+                color: accountType === 64 ? "white" : "#B71A18",
+              }}
               dangerouslySetInnerHTML={{
                 __html:
                   decodeHTML(basicPackage.package_detail) ||
@@ -110,15 +154,47 @@ const ManageAccount = () => {
             />
 
             <div className="text-start">
-              <p className={styles.price}>
+              <p
+                className={styles.BasicPrice}
+                style={{
+                  color: accountType === 64 ? "white" : "black",
+                }}
+              >
                 ${basicPackage.package_price || "350"}
               </p>
-              <p className={styles.priceLabel}>per year/pack</p>
+              <p
+                className={styles.BasicPriceLabel}
+                style={{
+                  color: accountType === 64 ? "white" : "black",
+                }}
+              >
+                per year/pack
+              </p>
             </div>
 
-            <button className={`mt-2 ${styles.customCurrentBtn}`}>
-              Current Package
-            </button>
+            {accountType === 64 ? (
+              <button
+                className={`mt-2 ${styles.customCurrentBtn}`}
+                style={{
+                  backgroundColor: "white",
+                  color: "#B71A18",
+                }}
+              >
+                Current Package
+              </button>
+            ) : (
+              <Button
+                variant="danger"
+                onClick={handleRedirect}
+                className={`px-5 ${styles.contactButton}`}
+                style={{
+                  backgroundColor: "#B71A18",
+                  color: "white",
+                }}
+              >
+                Contact Now →
+              </Button>
+            )}
           </div>
         </Col>
         {/* Right Side: Package Cards */}
@@ -129,17 +205,28 @@ const ManageAccount = () => {
           {/* Premium Package */}
           <div
             className={`${styles.packagePremiumCard} ${styles.premiumPackage}`}
+            style={{
+              backgroundColor: accountType === 64 ? "white" : "#B71A18", // Conditional color
+            }}
           >
             {/* icon  */}
             <Row className="my-1">
               <div className="text-start">
-                <img src={premiumIcon} alt="premium icon" />
+                <img
+                  src={accountType === 64 ? basicIcon : premiumIcon}
+                  alt="premium icon"
+                />
               </div>
             </Row>
 
             {/* package name  */}
             <Row className="my-1">
-              <h4 className={`mt-3 text-start ${styles.premiumPackageName}`}>
+              <h4
+                className={`mt-3 text-start ${styles.premiumPackageName}`}
+                style={{
+                  color: accountType === 64 ? "#B71A18" : "white", // Conditional color
+                }}
+              >
                 {premiumPackage.package_name || "Basic Package"}
               </h4>
             </Row>
@@ -147,7 +234,13 @@ const ManageAccount = () => {
             {/* feeatured  */}
             <Row className="mb-2">
               <div className="d-flex">
-                <button className={`${styles.customPremiumBtn}`}>
+                <button
+                  className={`${styles.customPremiumBtn}`}
+                  style={{
+                    backgroundColor: accountType === 64 ? "#B71A18" : "white",
+                    color: accountType === 64 ? "white" : "#B71A18",
+                  }}
+                >
                   Features
                 </button>
               </div>
@@ -157,6 +250,9 @@ const ManageAccount = () => {
             <Row>
               <p
                 className="text-start fs-6"
+                style={{
+                  color: accountType === 64 ? "#B71A18" : "white",
+                }}
                 dangerouslySetInnerHTML={{
                   __html:
                     decodeHTML(premiumPackage.package_detail) ||
@@ -168,23 +264,51 @@ const ManageAccount = () => {
             {/* price  */}
             <Row>
               <div className="text-start">
-                <p className={`text-start ${styles.PremiumPrice}`}>
+                <p
+                  className={`text-start ${styles.PremiumPrice}`}
+                  style={{
+                    color: accountType === 64 ? "black" : "white",
+                  }}
+                >
                   ${premiumPackage.package_price || "0"}
                 </p>
-                <p className={styles.PremiumPriceLabel}>per year/pack</p>
+                <p
+                  className={styles.PremiumPriceLabel}
+                  style={{
+                    color: accountType === 64 ? "black" : "white",
+                  }}
+                >
+                  per year/pack
+                </p>
               </div>
             </Row>
 
             {/* contact button  */}
             <Row>
               <div className="d-flex justify-content-center">
-                <Button
-                  variant="danger"
-                  onClick={handleRedirect}
-                  className={`px-5 ${styles.contactButton}`}
-                >
-                  Contact Now →
-                </Button>
+                {accountType === 64 ? (
+                  <Button
+                    variant="danger"
+                    onClick={handleRedirect}
+                    className={`px-5 ${styles.contactButton}`}
+                    style={{
+                      backgroundColor: "#B71A18",
+                      color: "white",
+                    }}
+                  >
+                    Contact Now →
+                  </Button>
+                ) : (
+                  <button
+                    className={`mt-2 ${styles.customCurrentBtn}`}
+                    style={{
+                      backgroundColor: "white",
+                      color: "#B71A18",
+                    }}
+                  >
+                    Current Package
+                  </button>
+                )}
               </div>
             </Row>
           </div>
