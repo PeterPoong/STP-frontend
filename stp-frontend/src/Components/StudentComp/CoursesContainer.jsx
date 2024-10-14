17978cd8b1baffb4963fa60194ca6139f5006a9d
poonghy <peterpoong2000@gmail.com>
2024-10-14 11:10:27 +0800

    hotpick courses category fixed

--- stp-frontend/src/Components/StudentComp/CoursesContainer.jsx
+++ stp-frontend/src/Components/StudentComp/CoursesContainer.jsx
@@ -11,7 +11,7 @@ import "swiper/css/navigation";
 import { Pagination, Navigation } from "swiper/modules";
 
 const baseURL = import.meta.env.VITE_BASE_URL;
-const apiURL = "http://192.168.0.70:8000/api/student/hotPickCategoryList"; // Updated API URL
+const apiURL = `${baseURL}api/student/hotPickCategoryList`; // Updated API URL
 
 const CoursesContainer = () => {
   const [buttons, setButtons] = useState([]);
