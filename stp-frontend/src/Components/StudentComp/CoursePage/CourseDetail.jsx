9cd8de4f7399d588964003cf35d2482ec73d5791
howardyam <33825761+howardyam@users.noreply.github.com>
2024-12-16 16:24:02 +0800

    16/12/2024
    -modify the code where the list of course not responsive in tablet screen size

--- stp-frontend/src/Components/StudentComp/CoursePage/CourseDetail.jsx
+++ stp-frontend/src/Components/StudentComp/CoursePage/CourseDetail.jsx
@@ -920,14 +920,26 @@ const CourseDetail = () => {
                           spaceBetween: 5,
                         },
                         640: {
-                          slidesPerView: 3,
-                          spaceBetween: 10,
+                          slidesPerView: 1,
+                          spaceBetween: 5,
                         },
                         768: {
-                          slidesPerView: 4,
-                          spaceBetween: 15,
+                          slidesPerView: 2,
+                          spaceBetween: 5,
                         },
                         1024: {
+                          slidesPerView: 3,
+                          spaceBetween: 5,
+                        },
+                        1254: {
+                          slidesPerView: 3,
+                          spaceBetween: 1,
+                        },
+                        1324: {
+                          slidesPerView: 3,
+                          spaceBetween: 1,
+                        },
+                        1488: {
                           slidesPerView: 5,
                           spaceBetween: 10,
                         },
